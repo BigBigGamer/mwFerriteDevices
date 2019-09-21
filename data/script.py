cfb80264a6bf97d4c4241abbc500f92a3c120823
Tallahassee <aps98@mail.ru>
2019-09-21 21:08:56 +0300

    Правки

--- data/script.py
+++ data/script.py
@@ -31,12 +31,69 @@ low_2_neg = [240,230,210,170,130,50,10,40,200,370,460]
 low_12_pos = [250,260,300,360,415,460,445,460,float('nan'),float('nan'),float('nan')]
 low_12_neg = [250,240,270,320,370,420,415,440,float('nan'),float('nan'),float('nan')]
 
+a1h = 4 
+a1l = 6 
+a2h = 6 
+a2l = 3.5
+a12h = 6
+a12l = 3.5 
+high_1_pos = 10**(a1h/10)*np.array(high_1_pos)
+high_1_neg = 10**(a1h/10)*np.array(high_1_neg)
+
+high_2_pos = 10**(a2h/10)*np.array(high_2_pos)
+high_2_neg = 10**(a2h/10)*np.array(high_2_neg)
+
+low_1_pos = 10**(a1l/10)*np.array(low_1_pos)
+low_1_neg = 10**(a1l/10)*np.array(low_1_neg)
+
+low_2_pos = 10**(a2l/10)*np.array(low_2_pos)
+low_2_neg = 10**(a2l/10)*np.array(low_2_neg)
+
+high_12_pos = 10**(a12h/10)*np.array(high_12_pos)
+high_12_neg = 10**(a12h/10)*np.array(high_12_neg)
+
+low_12_pos = 10**(a12l/10)*np.array(low_12_pos)
+low_12_neg = 10**(a12l/10)*np.array(low_12_neg)
+
+# pairs_pos = [[high_1_pos,low_1_pos],[high_2_pos,low_2_pos],[high_12_pos,low_12_pos]]
+# pairs_neg = [[high_1_pos,low_1_pos],[high_2_pos,low_2_pos],[high_12_pos,low_12_pos]]
+# pos_1_sum = (np.array(high_1_pos)+np.array(low_1_pos))/100
+# high_1_pos = high_1_pos/pos_1_sum
+# low_1_pos = low_1_pos/pos_1_sum
+
+# neg_1_sum = (np.array(high_1_neg)+np.array(low_1_neg))/100
+# high_1_neg = high_1_neg/neg_1_sum
+# low_1_neg = low_1_neg/neg_1_sum
+
+# pos_2_sum = (np.array(high_2_pos)+np.array(low_2_pos))/100
+# high_2_pos = high_2_pos/pos_2_sum
+# low_2_pos = low_2_pos/pos_2_sum
+
+# neg_2_sum = (np.array(high_2_neg)+np.array(low_2_neg))/100
+# high_2_neg = high_2_neg/neg_2_sum
+# low_2_neg = low_2_neg/neg_2_sum
+
+# pos_12_sum = (np.array(high_12_pos)+np.array(low_12_pos))/100
+# high_12_pos = high_12_pos/pos_12_sum
+# low_12_pos = low_12_pos/pos_12_sum
+
+# neg_12_sum = (np.array(high_12_neg)+np.array(low_12_neg))/100
+# high_12_neg = high_12_neg/neg_12_sum
+# low_12_neg = low_12_neg/neg_12_sum
+
 fig = plt.figure(figsize = (14,7))
 ax = fig.add_subplot(111)
-ax.plot(curr,high_12_pos,'ro-',label = 'Верхнее, I+')
-ax.plot(curr,high_12_neg,'ko-',label = 'Верхнее, I-')
-ax.plot(curr,low_12_pos,'rs--',label = 'Нижнее, I+')
-ax.plot(curr,low_12_neg,'ks--',label = 'Нижнее, I-')
+
+alpha = 6 #db
+# testing
+# P0_pos = ((np.array(high_1_pos)+np.array(low_1_pos)+np.array([alpha] * 11))/10)
+# testing
+# ax.plot(curr,P0_pos,'ro-',label = 'Верхнее, I+')
+
+ax.plot(curr,high_2_pos,'ro-',label = 'Верхнее, I+')
+ax.plot(curr,high_2_neg,'ko-',label = 'Верхнее, I-')
+ax.plot(curr,low_2_pos,'rs--',label = 'Нижнее, I+')
+ax.plot(curr,low_2_neg,'ks--',label = 'Нижнее, I-')
 # x_interpol = np.arange(0,70,0.05)
 # Interpol = sp.interpolate.CubicSpline(lnew,sqvnew,extrapolate = False)
 # ax.plot(x_interpol,Interpol(x_interpol),'r-')
@@ -49,4 +106,4 @@ plt.xlabel(r'I, a.u.',fontsize = 25)
 # plt.ylabel(r'$|E|^2$, $\mu V$')
 
 plt.show()
-# fig.savefig('imgs/graphs/phaser3.png',dpi=500)
+# fig.savefig('imgs/graphs/phaser1.png',dpi=500)
